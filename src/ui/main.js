9a93462e91fd819ee58c389374981e4cf9d389b2
Tim Limlei <58039156+blueqwertz@users.noreply.github.com>
2021-04-26 22:38:28 +0200

    settings

--- src/ui/main.js
+++ src/ui/main.js
@@ -16,22 +16,22 @@ var pieceStart = new Array(64)
 function setupBoard() {
     document.getElementById("boardbg").innerHTML = ""
     document.getElementById("boardbg").innerHTML += `<svg viewBox="0 0 100 100" class="coordinates">
-                    <text x="0.75" y="3.5" font-size="2.3" class="coordinate-light">8</text>
-                    <text x="0.75" y="15.75" font-size="2.3" class="coordinate-dark">7</text>
-                    <text x="0.75" y="28.25" font-size="2.3" class="coordinate-light">6</text>
-                    <text x="0.75" y="40.75" font-size="2.3" class="coordinate-dark">5</text>
-                    <text x="0.75" y="53.25" font-size="2.3" class="coordinate-light">4</text>
-                    <text x="0.75" y="65.75" font-size="2.3" class="coordinate-dark">3</text>
-                    <text x="0.75" y="78.25" font-size="2.3" class="coordinate-light">2</text>
-                    <text x="0.75" y="90.75" font-size="2.3" class="coordinate-dark">1</text>
-                    <text x="10" y="99" font-size="2.3" class="coordinate-dark">a</text>
-                    <text x="22.5" y="99" font-size="2.3" class="coordinate-light">b</text>
-                    <text x="35" y="99" font-size="2.3" class="coordinate-dark">c</text>
-                    <text x="47.5" y="99" font-size="2.3" class="coordinate-light">d</text>
-                    <text x="60" y="99" font-size="2.3" class="coordinate-dark">e</text>
-                    <text x="72.5" y="99" font-size="2.3" class="coordinate-light">f</text>
-                    <text x="85" y="99" font-size="2.3" class="coordinate-dark">g</text>
-                    <text x="97.5" y="99" font-size="2.3" class="coordinate-light">h</text>
+                    <text x="0.75" y="3.5" font-size="2.2" class="coordinate-light">8</text>
+                    <text x="0.75" y="15.75" font-size="2.2" class="coordinate-dark">7</text>
+                    <text x="0.75" y="28.25" font-size="2.2" class="coordinate-light">6</text>
+                    <text x="0.75" y="40.75" font-size="2.2" class="coordinate-dark">5</text>
+                    <text x="0.75" y="53.25" font-size="2.2" class="coordinate-light">4</text>
+                    <text x="0.75" y="65.75" font-size="2.2" class="coordinate-dark">3</text>
+                    <text x="0.75" y="78.25" font-size="2.2" class="coordinate-light">2</text>
+                    <text x="0.75" y="90.75" font-size="2.2" class="coordinate-dark">1</text>
+                    <text x="10" y="99" font-size="2.2" class="coordinate-dark">a</text>
+                    <text x="22.5" y="99" font-size="2.2" class="coordinate-light">b</text>
+                    <text x="35" y="99" font-size="2.2" class="coordinate-dark">c</text>
+                    <text x="47.5" y="99" font-size="2.2" class="coordinate-light">d</text>
+                    <text x="60" y="99" font-size="2.2" class="coordinate-dark">e</text>
+                    <text x="72.5" y="99" font-size="2.2" class="coordinate-light">f</text>
+                    <text x="85" y="99" font-size="2.2" class="coordinate-dark">g</text>
+                    <text x="97.5" y="99" font-size="2.2" class="coordinate-light">h</text>
                 </svg>`
     for (let i = 0; i < 64; i++) {
         let x = 7 - (i % 8)
